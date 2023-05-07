ff829fcfe30aea66a94c85f79dd680ae67fe0e1e
RAJ1 <appdevdj@>
2023-05-07 09:26:39 +0200

    Castomerv01

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import models.Customer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        System.out.println("Hello ouer New Customer");
 
-    //
+
+        Customer customer = new Customer("Jan", "Kowlski",
+                "jakkowalski@gmail.com", "+486543798");
+
+        System.out.println(customer);
+    }
 }
