e2eca75e115a09c5fd72d1ac693736c638675c50
j.pietrzak <jooannapietrzak@gamil.com>
2023-05-07 09:39:33 +0200

    AirPort wersja 01

--- src/main/java/models/AirPort.java
+++ src/main/java/models/AirPort.java
@@ -1,4 +1,27 @@
 package models;
 
 public class AirPort {
+
+            String miejscewylotu ;
+            String miejsceprzylotu;
+
+    public AirPort(String miejscewylotu) {
+        this.miejscewylotu = miejscewylotu;
+    }
+
+    public void setMiejsceprzylotu(String miejsceprzylotu) {
+        this.miejsceprzylotu = miejsceprzylotu;
+    }
+
+    public void setMiejscewylotu(String miejscewylotu) {
+        this.miejscewylotu = miejscewylotu;
+    }
+
+    public String getMiejsceprzylotu() {
+        return miejsceprzylotu;
+    }
+
+    public String getMiejscewylotu() {
+        return miejscewylotu;
+    }
 }
