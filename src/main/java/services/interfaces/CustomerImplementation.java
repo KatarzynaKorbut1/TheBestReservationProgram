1f59ff77a5b58b39c06a0707b2830616eec362ec
Katarzyna Korbut <katarzyna.korbut@outlook.com>
2023-05-08 20:54:15 +0200

    Admin Implementation - 7 Commit

--- src/main/java/services/interfaces/CustomerImplementation.java
+++ src/main/java/services/interfaces/CustomerImplementation.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-import static services.AdminImplementation.writeToFile;
+
 
 public class CustomerImplementation {
     Scanner scanner = new Scanner(System.in);
