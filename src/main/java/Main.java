f5d0cf3062b6f3486084a979ae2581886d9f3b9a
RAJ1 <appdevdj@>
2023-05-08 20:01:27 +0200

    wersja 01

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import services.AdminImplementation;
 import services.InvalidEmailException;
+import services.interfaces.CustomerImplementation;
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -7,8 +8,9 @@ import java.io.Serializable;
 public class Main implements Serializable {
     public static void main(String[] args) throws InvalidEmailException, IOException, ClassNotFoundException {
         AdminImplementation adminImplementation = new AdminImplementation();
-
         adminImplementation.newServiceAccount();
 
+        CustomerImplementation customerImplementation = new CustomerImplementation();
+        customerImplementation.newCustomerAccount();
     }
 }
